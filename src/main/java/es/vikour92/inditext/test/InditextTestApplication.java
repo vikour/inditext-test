4056136b6ee596a24a170baed4e22d616074ce4a
Víctor Manuel Ortiz Guardeño <vikour92@gmail.com>
2023-12-15 05:02:16 -0600

    Application created

---
+++ src/main/java/es/vikour92/inditext/test/InditextTestApplication.java
@@ -0,0 +1,13 @@
+package es.vikour92.inditext.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditextTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditextTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/vikour92/inditext/test/InditextTestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.vikour92.inditext.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditextTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
