a6ba94ae0bed9d90131bf74cde234d9779a3aa14
Víctor Manuel Ortiz Guardeño <vikour92@gmail.com>
2023-12-19 01:28:19 -0600

    TEST SUCCESS: PriceServiceTest.testFind_whenPriorityCollision_thenCorrect

--- src/test/java/es/vikour92/inditext/test/domain/service/PriceServiceTest.java
+++ src/test/java/es/vikour92/inditext/test/domain/service/PriceServiceTest.java
@@ -8,14 +8,12 @@ import es.vikour92.inditext.test.domain.model.Product;
 import es.vikour92.inditext.test.domain.ports.PricePersistencePort;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -156,6 +154,46 @@ class PriceServiceTest {
         assertTrue(mayATopPrice.isPresent());
         Price topPrice = mayATopPrice.get();
         assertEquals(priceC, topPrice);
+    }
+
+
+    @Test
+    public void testFind_whenPriorityCollision_thenCorrect() {
+        TestParam tp = TestParam.newDefault();
+        Price.Builder priceBuilder = Price.builder()
+                .startDate(tp.dateInterval.start())
+                .endDate(tp.dateInterval.end())
+                .product(tp.product);
+
+        // Only A and B has the same priority, so some of them are price candidates.
+        Price priceA = priceBuilder
+                .id(1L).priority(5L)
+                .amount(BigDecimal.ONE)
+                .build();
+        Price priceB = priceBuilder
+                .id(1L).priority(5L)
+                .amount(BigDecimal.TEN)
+                .build();
+        Price priceC = priceBuilder
+                .id(1L).priority(2L)
+                .amount(BigDecimal.valueOf(1024))
+                .build();
+        List<Price> pricesInBD = List.of(priceA, priceB, priceC);
+
+        // Mocks
+        when(pricePersistencePort.find(eq(tp.brandId()), eq(tp.productId()), eq(tp.dateInterval)))
+                .thenReturn(pricesInBD);
+
+        // test
+        Optional<Price> mayATopPrice = priceService.find(tp.brandId(), tp.productId(), tp.dateInterval);
+
+        // checks
+        assertNotNull(mayATopPrice);
+        assertTrue(mayATopPrice.isPresent());
+        Price topPrice = mayATopPrice.get();
+
+        // It's not defined which price, A or B, must return the service. Either of them.
+        assertTrue(Objects.equals(topPrice, priceA) || Objects.equals(topPrice, priceB));
     }
 
     private DomainEntityNotFoundException buildException(Class<?> entityClass, long id) {
