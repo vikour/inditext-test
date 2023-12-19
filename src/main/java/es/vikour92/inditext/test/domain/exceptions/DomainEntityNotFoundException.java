abd664601ce740b645600e3367de246d2b3a2644
Víctor Manuel Ortiz Guardeño <vikour92@gmail.com>
2023-12-19 01:51:13 -0600

    Refactor: setted to final DomainEntityNotFoundException fields

--- src/main/java/es/vikour92/inditext/test/domain/exceptions/DomainEntityNotFoundException.java
+++ src/main/java/es/vikour92/inditext/test/domain/exceptions/DomainEntityNotFoundException.java
@@ -2,8 +2,8 @@ package es.vikour92.inditext.test.domain.exceptions;
 
 public class DomainEntityNotFoundException extends PersistenceException {
     private static final String DEFAULT_MSG = "Could not be found entity class '%s' with ID : '%s'";
-    private Class<?> domainClass;
-    private Object id;
+    private final Class<?> domainClass;
+    private final Object id;
     public DomainEntityNotFoundException(String message, Class<?> domainClass, Object id, Throwable cause) {
         super(message, cause);
         this.domainClass = domainClass;
